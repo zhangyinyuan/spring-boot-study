4d33cc6c74a0feeddbf09b410d0718a4f83fdffc
Your Name <269@qq.com>
2017-12-30 17:06:10 +0800

    add monitor

---
+++ spring-boot-study-web/src/main/java/com/yuan/ngu/springbootstudyweb/controller/MonitorController.java
@@ -0,0 +1,19 @@
+package com.yuan.ngu.springbootstudyweb.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MonitorController {
+
+
+    @RequestMapping(path = "/monitor")
+    @ResponseBody
+    public Object monitor() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result","success");
+        return jsonObject;
+    }
+}
