35253dcf824bbfd49dd4e0da689f8bc99b32c935
Your Name <269@qq.com>
2017-12-30 21:49:00 +0800

    加入 produces

--- spring-boot-study-web/src/main/java/com/yuan/ngu/springboot/controller/RedisController.java
+++ spring-boot-study-web/src/main/java/com/yuan/ngu/springboot/controller/RedisController.java
@@ -6,18 +6,20 @@ import com.yuan.ngu.springboot.RedisService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/redis")
+@Api(value = "Redis测试接口",description = "仅仅用来测试，没有其他用途")
 public class RedisController {
 
     @Reference(version = "1.0.0")
     RedisService redisService;
 
     @ApiOperation(value = "获取Redis", notes = "用来测试Redis")
-    @ApiImplicitParam(paramType = "path",name = "key", value = "Redis 的 key", required = true,dataType = "string")
-    @RequestMapping(value = "/get/{key}", method = RequestMethod.GET)
+    @ApiImplicitParam(paramType = "path",name = "key", defaultValue = "dashabi",value = "Redis 的 key", required = true,dataType = "string")
+    @RequestMapping(value = "/get/{key}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public Object get(@PathVariable String key) {
         String redisValue = (String) redisService.opsForValue(key);
