138fbef81478dc88997ef66732b83d6db8f81fc7
Your Name <269@qq.com>
2017-12-30 23:18:48 +0800

    测试Redis的写入字符串

--- spring-boot-study-web/src/main/java/com/yuan/ngu/springboot/controller/RedisController.java
+++ spring-boot-study-web/src/main/java/com/yuan/ngu/springboot/controller/RedisController.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.yuan.ngu.springboot.RedisService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,22 @@ public class RedisController {
     @RequestMapping(value = "/get/{key}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public Object get(@PathVariable String key) {
+        String redisValue = redisService.getStr(key);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("value", redisValue);
+        jsonObject.put("key", key);
+        return jsonObject;
+    }
+
+    @ApiOperation(value = "redis中写入字符串", notes = "redis中写入字符串")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "string", name = "key", value = "Redis key", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "string", name = "value", value = "Redis  string value", required = true)
+    })
+    @RequestMapping(value = "/set/{key}/{value}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public Object set(@PathVariable String key,@PathVariable String value) {
+        redisService.setStr(key,value);
         String redisValue = redisService.getStr(key);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("value", redisValue);
